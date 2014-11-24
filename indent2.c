fef58fa831c8c36a5c11166a5a7fe90a372c067b
pavlosts <pavlos_tsaou@hotmail.gr>
2014-11-24 16:06:01 +0200

    Fixed spaces
    Now it prints only one space

--- indent2.c
+++ indent2.c
@@ -5,9 +5,19 @@ int main(){
 	int count2 = 0;												//Counts open parenthesis
 	int flag = 0;												//True if ch is inside ''
 	char ch;													//Character being read by getchar()
+	int flag2 = 0;												//True if previous character printed is space
 	int i;
 	
 	while((ch = getchar()) != EOF){
+		if( ch == ' '){
+			flag2++;
+			if(flag2 == 1){
+				printf(" ");
+			}
+		}else{
+			flag2 = 0;
+		}
+		
 		if(ch == '{'){
 			count++;
 			printf("\n");
@@ -15,24 +25,32 @@ int main(){
 			count--;
 			//printf("\n");
 		}
+		
 		if(ch == '\''){
 			flag = !flag;
 		}
+		
 		if( ch == '(' ){
 			count2++;
 		}else if( ch == ')'){
 			count2--;
 		}
+		
 		if( (ch == ';' || ch == '{') && ch != '}' ){
 			printf("%c",ch);
+			
 			if(flag == 0 && count2 == 0){
 				printf("\n");
+				
 				for(i = 1; i <= count; i++){
 					printf("\t");
 				}
 			}
 		}else{
-			printf("%c",ch);
+			
+			if(flag2 == 0){
+				printf("%c",ch);
+			}
 		}
 	}
 }

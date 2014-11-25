5386848797a96f23b7734a3b4ac47079f0fab309
pavlosts <pavlos_tsaou@hotmail.gr>
2014-11-25 12:57:01 +0200

    More additions
    Added instructions for symbols \t, \n, \\, \", \'

--- indent.c
+++ indent.c
@@ -1,14 +1,41 @@
 #include <stdio.h>
 
 int main(){
+	int i;
 	int count = 0;												//Counts curly braces
 	int count2 = 0;												//Counts open parenthesis
 	int flag = 0;												//True if ch is inside ''
 	char ch;													//Character being read by getchar()
-	int flag2 = 0;												//True if previous character printed is space
-	int i;
+	int flag2 = 0;												//True if previous character was space
+	int flag3 = 0;												//True if previous character was tab
+	int flag4 = 0;												//True if ch is inside " "
+	int flag5 = 0;												//True if ch is \	
 	
 	while((ch = getchar()) != EOF){
+		
+		if( ch == '\n'){
+			continue;
+		}
+		
+		if( ch == '\t'){
+			flag3++;
+			if(flag3 == 1){
+				printf("\t");
+			}
+		}else{
+			flag3=0;
+		}
+		
+		if( ch == '\\'){
+			flag5 = !flag5;
+		}
+		
+		if( ch == '\"' ){
+			if( flag5 != 0){
+				flag4 = !flag4;
+			}
+		}
+		
 		if( ch == ' '){
 			flag2++;
 			if(flag2 == 1){
@@ -18,16 +45,20 @@ int main(){
 			flag2 = 0;
 		}
 		
+		if(ch == '\''){
+			if(flag5 != 0){
+				flag = !flag;
+			}
+		}
+		
 		if(ch == '{'){
 			count++;
-			printf("\n");
+			if(flag == 0 && flag4 == 0){
+				printf("\n");
+			}
 		}else if(ch == '}'){
 			count--;
 			//printf("\n");
-		}
-		
-		if(ch == '\''){
-			flag = !flag;
 		}
 		
 		if( ch == '(' ){
@@ -36,10 +67,10 @@ int main(){
 			count2--;
 		}
 		
-		if( (ch == ';' || ch == '{') && ch != '}' ){
+		if( ch == ';' || ch == '{' || ch == '}' ){
 			printf("%c",ch);
 			
-			if(flag == 0 && count2 == 0){
+			if(flag == 0 && count2 == 0 && flag4 == 0){
 				printf("\n");
 				
 				for(i = 1; i <= count; i++){
